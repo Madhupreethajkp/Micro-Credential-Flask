d4f2b273618cae37eda3f7f3ec13466d09f33e13
Madhupreetha Janakiraman <46986542+Madhupreethajkp@users.noreply.github.com>
2020-07-31 17:30:41 +0530

    Add files via upload

--- models/user.py
+++ models/user.py
@@ -1,66 +1,185 @@
-from db import db
+import random
+import sqlite3
 
 
-class UserModel(db.Model):
-    __tablename__ = 'user_details'
+import data as data
 
-    first_name = db.Column(db.String(80))
-    last_name = db.Column(db.String(80))
-    date_of_birth = db.Column(db.String(80))
-    address = db.Column(db.String(100))
-    contact_no = db.Column(db.Integer)
-    email = db.Column(db.String(80), primary_key=True)
-    qualification = db.Column(db.String(80))
-    gender = db.Column(db.String(80))
-    salary = db.Column(db.Integer)
-    pan_no = db.Column(db.String(80))
-    type_of_employer = db.Column(db.String(100))
-    name_of_employer = db.Column(db.String(100))
-    user_id = db.Column(db.String(100))
-    password = db.Column(db.String(100))
+from flask_jwt import jwt_required
+from flask_mail import Message, Mail
+from flask_restful import Resource, reqparse
 
-    def __init__(self, first_name, last_name, date_of_birth, address, contact_no, email, qualification, gender, salary,
-                 pan_no, type_of_employer, name_of_employer, user_id, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.date_of_birth = date_of_birth
-        self.address = address
-        self.contact_no = contact_no
-        self.email = email
-        self.qualification = qualification
-        self.gender = gender
-        self.salary = salary
-        self.pan_no = pan_no
-        self.type_of_employer = type_of_employer
-        self.name_of_employer = name_of_employer
-        self.user_id = user_id
-        self.password = password
+#from app import app
+from models.user import UserModel
 
-    def json(self):
-        return {'first_name': self.first_name,
-                'last_name': self.last_name,
-                'date_of_birth': self.date_of_birth,
-                'address': self.address,
-                'contact_no': self.contact_no,
-                'email': self.email,
-                'qualification': self.qualification,
-                'gender': self.gender,
-                'salary': self.salary,
-                'pan_no': self.pan_no,
-                'type_of_employer': self.type_of_employer,
-                'name_of_employer': self.name_of_employer,
-                'user_id': self.user_id,
-                'password': self.password
-                }
+from datetime import date
+from random import randint
+from urllib.parse import quote
+import webbrowser
+#mail = Mail(app)
+today = date.today()
 
-    @classmethod
-    def find_by_email(cls, email):
-        return cls.query.filter_by(email=email).first()
 
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+class UserRegister(Resource):
+    TABLE_NAME = 'user_new'
 
-    def delete_from_db(self):
-        db.session.delete(self)
-        db.session.commit()
+    parser = reqparse.RequestParser()
+    parser.add_argument('first_name',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('last_name',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('date_of_birth',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('address',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('contact_no',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('email',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('qualification',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('gender',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('salary',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('pan_no',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('type_of_employer',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('name_of_employer',
+                        type=str,
+                        required=True,
+
+                        )
+
+    @jwt_required()
+    def get(self, email):
+        user = UserModel.find_by_email(email)
+        if user:
+            return user.json()
+        return {'message': 'user not found'}, 404
+
+    @jwt_required()
+    def get(self, user_id):
+        user = UserModel.find_by_user_id(user_id)
+        if user:
+            return user.json()
+        return {'message': 'user not found'}, 404
+
+    @jwt_required()
+    def get(self, user_id, password):
+        user = UserModel.find_by_login(user_id, password)
+        if user:
+            return user.json()
+        return {'message': 'user not found'}, 404
+
+    @jwt_required()
+    def get(self, password):
+        user = UserModel.find_by_password(password)
+        if user:
+            return user.json()
+        return {'message': 'user not found'}, 404
+
+
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if UserModel.find_by_email(data['email']):
+            return {"message": "User with that email id already exists."}, 400
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        income = int(data['salary'])
+        salary_per_year = income*12
+        print(salary_per_year)
+        print(type(salary_per_year))
+        if int(salary_per_year) <= 500000:
+            user_type_id = 'A'
+        elif int(salary_per_year) > 500000 & int(salary_per_year) <= 1000000:
+            user_type_id = 'B'
+        elif int(salary_per_year) > 1000000 & int(salary_per_year) <= 1500000:
+            user_type_id = 'C'
+        elif int(salary_per_year) > 1500000 & int(salary_per_year) <= 3000000:
+            user_type_id = 'D'
+        elif int(salary_per_year) > 3000000:
+            user_type_id = 'E'
+        print(user_type_id)
+
+       # n = str(cursor.execute("select last_insert_rowid() from auto"))
+        #print(type(n))
+        #print(n)
+       # num = 1200 + int(n)
+
+        #user_id = user_type_id + '-' + str(num)
+        num =1200
+        #num = 1200 + cursor.lastrowid
+        #print(type(cursor.lastrowid))
+        print(num)
+        user_id = user_type_id + '-' + str(num)
+        date = today.strftime("%d")
+
+        month = today.strftime("%B")
+
+        random_number = randint(100, 999)
+        character = random.choice('$_')
+        password = str(date) + str(month) + str(character) + str(random_number)
+        #msg = Message('Dear User,', sender='madhupreetha98@gmail.com', recipients=[data['email']])
+        #msg.body = "Your user id is " + data['user_id'] + " and your password is " + data['password']
+        query = "INSERT INTO {table} VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?)".format(
+            table=self.TABLE_NAME)
+        cursor.execute(query, (data['first_name'], data['last_name'], data['date_of_birth'], data['address'], data['contact_no'], data['email'], data['qualification'], data['gender'], data['salary'], data['pan_no'],
+                       data['type_of_employer']
+                       , data['name_of_employer'], user_id, password))
+        connection.commit()
+              # self.update(data['email'])
+
+        query1 = "UPDATE user_tb set user_id=user_id and password = password"
+        cursor.execute(query1)
+
+        connection.commit()
+
+        connection.close()
+
+       # mail.send(msg)
+
+        return {"message": "User created successfully."}, 201
+
+
+
+
+
+class UserList(Resource):
+      def get(self):
+           return {'users': list(map(lambda x: x.json(), UserModel.query.all()))}
