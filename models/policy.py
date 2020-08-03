552c569acd7e432bcb01822cf1dd0df15d9a2cf3
Madhupreetha Janakiraman <46986542+Madhupreethajkp@users.noreply.github.com>
2020-08-03 09:42:25 +0530

    Update policy.py

--- models/policy.py
+++ models/policy.py
@@ -1,143 +1,84 @@
-import sqlite3
-from datetime import date
-
-from flask_jwt import jwt_required
-from flask_restful import Resource, reqparse
-
-from models.policy import PolicyModel
-from resources.user import today
+from db import db
 
 
-class PolicyRegister(Resource):
-    TABLE_NAME = 'policy_table'
+class PolicyModel(db.Model):
+    __tablename__ = 'policy_table'
 
-    parser = reqparse.RequestParser()
-
-    parser.add_argument('policy_name',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('start_date',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('duration_in_years',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('company_name',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('initial_deposit',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('policy_type',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('user_type',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('terms_per_year',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('term_amount',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('interest',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-
-    @jwt_required()
-    def get(self, policy_name):
-        policy = PolicyModel.find_by_policy_name(policy_name)
-        if policy:
-            return policy.json()
-        return {'message': 'policy not found'}, 404
-
-    @jwt_required()
-    def get(self, company_name):
-        policy = PolicyModel.find_by_company_name(company_name)
-        if policy:
-            return policy.json()
-        return {'message': 'policy not found'}, 404
+    policy_name = db.Column(db.String(80), primary_key=True)
+    start_date = db.Column(db.String(80))
+    duration_in_years = db.Column(db.String(80))
+    company_name = db.Column(db.String(100))
+    initial_deposit = db.Column(db.Integer)
+    policy_type = db.Column(db.String(80))
+    user_type = db.Column(db.String(80))
+    terms_per_year = db.Column(db.Integer)
+    term_amount = db.Column(db.Integer)
+    interest = db.Column(db.Float)
+    maturity_amount = db.Column(db.Float)
+    policy_id = db.Column(db.String(120))
 
 
-    @jwt_required
-    def get_type(self, policy_type):
-        policy = PolicyModel.find_by_policy_type(policy_type)
-        if policy:
-            return policy.json()
-        return {'message': 'policy not found'}, 404
+    def __init__(self,  policy_name, start_date, duration_in_years, company_name, initial_deposit, policy_type, user_type, terms_per_year, term_amount,
+                 interest, maturity_amount, policy_id):
 
-    @jwt_required
-    def get_type(self, policy_id):
-        policy = PolicyModel.find_by_policy_id(policy_id)
-        if policy:
-            return policy.json()
-        return {'message': 'policy not found'}, 404
+        self.policy_name = policy_name
+        self.start_date = start_date
+        self.duration_in_years = duration_in_years
+        self.company_name = company_name
+        self.initial_deposit = initial_deposit
+        self.policy_type = policy_type
+        self.user_type = user_type
+        self.terms_per_year = terms_per_year
+        self.term_amount = term_amount
+        self.interest = interest
+        self.maturity_amount = maturity_amount
+        self.policy_id = policy_id
 
-    @jwt_required
-    def get_years(self, duration_in_years):
-        policy = PolicyModel.find_by_years(duration_in_years)
-        if policy:
-            return policy.json()
-        return {'message': 'policy not found'}, 404
+    def json(self):
+        return{
 
-    def post(self):
-        data = PolicyRegister.parser.parse_args()
+            'policy_name': self.policy_name,
+            'start_date': self.start_date,
+            'duration_in_years': self.duration_in_years,
+            'company_name': self.company_name,
+            'initial_deposit': self.initial_deposit,
+            'policy_type': self.policy_type,
+            'user_type': self.user_type,
+            'terms_per_year': self.terms_per_year,
+            'term_amount': self.term_amount,
+            'interest': self.interest,
+            'maturity_amount': self.maturity_amount,
+            'policy_id': self.policy_id
 
-        connection = sqlite3.connect('data.db')
-        print(data['policy_type'])
-        cursor = connection.cursor()
-        if data['policy_type'] == 'Vehicle Insurance':
-            policy_type_id = 'VI'
-        elif data['policy_type'] == 'Travel Insurance':
-            policy_type_id = 'TI'
-        elif data['policy_type'] == 'Health Insurance':
-            policy_type_id = 'HI'
-        elif data['policy_type'] == 'Life Insurance':
-            policy_type_id = 'LI'
-        elif data['policy_type'] == 'Child Plans':
-            policy_type_id = 'CP'
-        elif data['policy_type'] == 'Retirement Plans':
-            policy_type_id = 'RT'
-        print(type(policy_type_id))
-        i = 1
-        num = 4+i
-        year_of_start_date = today.strftime("%Y")
-        print(type(year_of_start_date))
-        policy_id = policy_type_id + '-' + year_of_start_date + '-' + '00' + str(num)
-        x = int(data['duration_in_years'])*int(data['terms_per_year'])*int(data['term_amount'])
-        y = int(data['interest'])/100
-        maturity_amount = int(data['initial_deposit'])+x+(x*y)
-        print(maturity_amount)
-        query = "INSERT INTO {table} VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?,?,?)".format(table=self.TABLE_NAME)
-        cursor.execute(query, (data['policy_name'], data['start_date'], data['duration_in_years'], data['company_name'], data['initial_deposit'], data['policy_type'], data['user_type'], data['terms_per_year'], data['term_amount'], data['interest'],maturity_amount, policy_id))
-        connection.commit()
-        connection.close()
+        }
 
-        return {"message": "Policy created successfully."}, 201
+    @classmethod
+    def find_by_policy_name(cls, policy_name):
+        return cls.query.filter_by(policy_name=policy_name).first()
+
+    @classmethod
+    def find_by_company_name(cls, company_name):
+        return cls.query.filter_by(company_name=company_name).first()
+
+    @classmethod
+    def find_by_policy_id(cls, policy_id):
+        return cls.query.filter_by(policy_id=policy_id).first()
+
+    @classmethod
+    def find_by_policy_type(cls, policy_type):
+        return cls.query.filter_by(policy_type=policy_type).get()
+
+    @classmethod
+    def find_by_years(cls, duration_in_years):
+        return cls.query.filter_by(duration_in_years=duration_in_years).get()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_to_db(self):
+        db.session.delete(self)
+        db.session.commit()
 
 
-class PolicyList(Resource):
-    def get(self):
-        return {'policies': list(map(lambda x: x.json(), PolicyModel.query.all()))}
-
-
+        
